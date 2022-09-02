0cd5a8daf6c12d15e1a3d6a8e935766be64e4ee2
Pablo Moreno <pablomoreno.inf@gmail.com>
2022-09-02 14:01:49 +0200

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/users/', include('users.api.v1.urls')),
     path('api/v1/wallets/', include('wallets.api.v1.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
